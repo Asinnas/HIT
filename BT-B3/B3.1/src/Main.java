f6be45d16d4b7c1b560758e09d89d96bebab292f
Asinnas <sanvinhfx@gmail.com>
2025-07-27 08:57:02 +0700

    b3

--- BT-B3/B3.1/src/Main.java
+++ BT-B3/B3.1/src/Main.java
@@ -5,28 +5,33 @@ public class Main {
     static final String triangle = "Triangle";
     static final String circle = "Circle";
     public static Scanner sc = new Scanner(System.in);
+    public static void tsquare(Scanner sc){
+        System.out.print("Nhap canh hinh vuong: ");
+        double a = sc.nextDouble();
+        System.out.printf("Dien tich hinh vuong la: %.2f\n", (a*a));
+    }
+    public static void ttriangle(Scanner sc){
+        System.out.print("Nhap day hinh tam giac: ");
+        double b = sc.nextDouble();
+        System.out.print("Nhap chieu cao hinh tam giac: ");
+        double h = sc.nextDouble();
+        System.out.printf("Dien tich hinh tam giac la: %.2f\n", (0.5 * b * h));
+    }
+    public static void tcircle(Scanner sc){
+        System.out.print("Nhap ban kinh hinh tron: ");
+        double r = sc.nextDouble();
 
+        System.out.printf("Dien tich hinh tron la: %.2f\n", (r*r*3.1415));
+    }
     public static void main(String[] args){
         System.out.print("Nhap hinh dang: ");
         String type = sc.nextLine();
-        double s;
         if (type.equalsIgnoreCase(square)){
-            System.out.print("Nhap canh hinh vuong: ");
-            double a = sc.nextDouble();
-            s = a * a;
-            System.out.printf("Dien tich hinh vuong la: %.2f\n", s);
+            tsquare(sc);
         }else if (type.equalsIgnoreCase(triangle)){
-            System.out.print("Nhap day hinh tam giac: ");
-            double b = sc.nextDouble();
-            System.out.print("Nhap chieu cao hinh tam giac: ");
-            double h = sc.nextDouble();
-            s = 0.5 * b * h;
-            System.out.printf("Dien tich hinh tam giac la: %.2f\n", s);
+            ttriangle(sc);
         }else if (type.equalsIgnoreCase(circle)){
-            System.out.print("Nhap ban kinh hinh tron: ");
-            double r = sc.nextDouble();
-            s = r*r*3.1415;
-            System.out.printf("Dien tich hinh tron la: %.2f\n", s);
+            tcircle(sc);
         }else{
             System.out.println("Hinh dang khong hop le");
         }

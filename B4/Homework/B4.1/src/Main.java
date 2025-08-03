3e40488d6895ebf168d31ecd3ca0cf158e64650d
Asinnas <sanvinhfx@gmail.com>
2025-08-03 15:15:46 +0700

    b4

--- B4/Homework/B4.1/src/Main.java
+++ B4/Homework/B4.1/src/Main.java
@@ -1,8 +1,58 @@
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
+        boolean t = true;
+        Student[] a = new Student[5];
+        while (t) {
+            System.out.println("\nNhap: Nhập sinh viên");
+            System.out.println("Chon: Chọn sinh viên");
+            System.out.println("Thoat: Thoát chu trình");
+            System.out.print("\nBạn chọn: ");
+            String chon = sc.nextLine();
 
-        System.out.printf("Hello and welcome!");
+            switch (chon){
+                case "Nhap":
+                for (int i = 0; i < 5; i++) {
+                    a[i] = new Student();
+                    System.out.println("\nSinh viên số " + (i+1));
 
+                    System.out.print("\nNhập tên: ");
+                    a[i].name = sc.nextLine();
 
+                    System.out.print("Nhập năm sinh: ");
+                    a[i].born = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.print("Nhập địa chỉ: ");
+                    a[i].address = sc.nextLine();
+
+                    System.out.print("Nhập điểm TX1: ");
+                    a[i].point_1 = sc.nextDouble();
+
+                    System.out.print("Nhập điểm TX2: ");
+                    a[i].point_2 = sc.nextDouble();
+
+                    System.out.print("Nhập điểm KTHP: ");
+                    a[i].point_final = sc.nextDouble();
+
+                    System.out.print("Nhập số tiết nghỉ: ");
+                    a[i].rest = sc.nextInt();
+                    sc.nextLine();
+                }
+                break;
+                case "Chon":
+                    System.out.print("Sinh viên số: ");
+                    int s = sc.nextInt();
+                    if (s >= 1 && s <= 5 && a[s - 1] != null){
+                        a[s - 1].list();
+                    } else System.out.print("Không có sinh viên này");
+                    break;
+                case "Thoat":
+                    t = false;
+                    break;
+            }
+        }
     }
 }
--- B4/Homework/B4.1/src/Student.java
+++ B4/Homework/B4.1/src/Student.java
@@ -1,10 +1,17 @@
 public class Student {
     String name;
-    int age;
+    int born;
     String address;
-    int point_1;
-    int point_2;
-    int point_final;
+    Double point_1;
+    Double point_2;
+    Double point_final;
     int rest;
+
+    public void list(){
+        int age = 2025 - born;
+        double GPA = point_1 * 0.2 + point_2 * 0.3 + point_final * 0.5;
+        System.out.printf("Ten: %s | Tuổi: %d | Địa chỉ: %s | TX1: %.2f | TX2: %.2f | KTHP: %.2f | GPA: %.2f | Nghỉ: %d buoi\n",
+                name, age, address, point_1, point_2, point_final,GPA, rest);
+    }
 }
 
